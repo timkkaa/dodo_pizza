aeadc42436eafe4173e084ca0016a3e56fb4fca7
timkkaa <karaevtimur200@gmail.com>
2024-11-16 13:51:51 +0600

    ...

--- dodo_pizza_core/products/models.py
+++ dodo_pizza_core/products/models.py
@@ -1,7 +1,19 @@
 from django.db import models
 
-class Pizza(models.Model):
-    name = models.CharField(max_length=250)
-    price = models.SmallIntegerField()
-    image = models.ImageField()
+from django.db import models
 
+class Pizza(models.Model):
+    """
+    Модель для пицц
+    """
+    name = models.CharField(max_length=255)
+    price = models.PositiveIntegerField(help_text="Указывайте цену в сомах")
+    image = models.ImageField(upload_to="pizza_images/")
+    consist = models.TextField(verbose_name="Состав")
+    is_new = models.BooleanField()
+    size = models.PositiveSmallIntegerField()
+    weight = models.PositiveSmallIntegerField(verbose_name="Вес в граммах")
+
+    class Meta:
+        verbose_name_plural = "Пиццы"
+        verbose_name = "Пицца"
